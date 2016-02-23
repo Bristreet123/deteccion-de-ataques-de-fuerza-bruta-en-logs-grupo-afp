02e7ec3c87ec0efa0bf34a761588c4ea9325bae3
miguelf11 <miguelachof11@gmail.com>
2016-02-22 23:33:40 -0430

    V6

--- Tarea.2.R
+++ Tarea.2.R
@@ -1,48 +1,78 @@
 library('dplyr')
+library('arules')
 #setwd("./Asignacion_2")
 data <- read.csv("data.csv", header = TRUE)
 
 data.log <- data
-#data.log$start_time <- as.POSIXct(data.log$start_time, origin = "1970-01-01")
+
+data.log$start_time <- as.POSIXct(data.log$start_time, origin = "1970-01-01")
 
 #2/3 de data random para Training
 aux <- data.log[data.log$asn == "1e2eb", ]
 sample.a <- sample_n(aux,size = (2/3) *nrow(aux))
 aux <- data.log[data.log$asn != "1e2eb",] 
 sample.b<- sample_n(aux,size = (2/3) * nrow(aux))
-log.training<- bind_rows(sample.a,sample.b)
+log.training<- rbind(sample.a,sample.b)
+
 
 
 #ordenamos por ip fuente y por tiempo 
 
-log.training <- log.training[order(log.training$source_ip, -log.training$start_time),]
+log.training <- log.training[order(log.training$start_time), ]
+
+
+#calcular columna intertime
+intertime <- diff.POSIXt(log.training$start_time)
+intertime <- c(0,intertime)
+log.training$intertime <- intertime
+
+#Q: con que valores discretizamos los valores hallados de "intertime"
+#R: Con la mediana porque es un valor confiable que no es tan afectado por outliers
+
+
+log.training[["intertime"]] <- ordered(cut(log.training[["intertime"]],
+                                           c(-Inf,0,median(log.training$intertime[log.training$intertime > 0]),Inf),
+                                       labels = c("none","low", "high")))
+log.training[["num_bytes"]] <- ordered(cut(log.training[["num_bytes"]],
+                                           c(-Inf,0,median(log.training$num_bytes[log.training$num_bytes > 0]),Inf),
+                                           labels = c("none","low", "high")))
+
+
+log.t<-log.training[-c(3,4,5,6,9,10)]
+log.t <- log.training[c(1,2,10,11)]
+trans <- as(log.t, "transactions")
+trans
+
+
+summary(trans)
+
+#columnas : elementos que están en esa "cesta" , ips , productos,atributos,etc..
+#filas : representan transacciones , individuos , compras , etc..
+
+itemFrequencyPlot(trans, support = 0.1, cex.names=0.8)
+
+rules <- apriori(trans,parameter = list(support = 0.01, confidence = 0.6))
+summary(rules)
+
+
+rules.intertimeNone <- subset(rules, subset = rhs %in% "num_bytes=low","intertime=none" & lift > 1.2)
+
+rules.intertimeNone <- subset(rules, subset = rhs %in% "intertime=none" & lift > 1.2)
+resultado <- inspect(head(sort(rules.intertimeNone, by = "confidence"), n = 3))
 
 
 
-#aquí dividimos el dataset entre los source_ip y luego 
-#calculamos la columna intertime para luego discretizar y usar
-#reglas de asociacion
-
-
-#tratar de separar la data
-a<- unique(data.log$source_ip)
-a[order(a)]
-length(a)
 
 
 
 
-#probando con sample de 10 (ejemplo de como crear intertime)
-training.sample <- sample_n(log.training,size = 10)
-training.sample <- training.sample[order(training.sample$source_ip, 
-                                         -training.sample$start_time),]
 
-dts <- as.POSIXlt(training.sample$start_time, origin = "1970-01-01")
-dts <- dts[order(dts)]
-#diferencia en minutos
-dif <- diff.POSIXt(dts)
-dif <- c(0,dif)
-training.sample$intertime <- dif
+
+
+
+
+
+
 
 
 
